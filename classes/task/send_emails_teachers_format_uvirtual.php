abe6030897f52ea7b5413d5c098c6df43a3c1f12
Ivan G. <iva.gar.veg@gmail.com>
2024-09-27 10:36:42 -0500

    add rdirecprogram role

--- classes/task/send_emails_teachers_format_uvirtual.php
+++ classes/task/send_emails_teachers_format_uvirtual.php
@@ -15,30 +15,36 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 namespace local_uvirtual\task;
+
+use course_info;
+use dml_exception;
+
 require_once($CFG->dirroot . '/course/format/uvirtual/lib.php');
 require_once($CFG->dirroot . '/blocks/grade_overview/classes/course_info.php');
 require_once($CFG->dirroot . '/blocks/grade_overview/lib.php');
 
 defined('MOODLE_INTERNAL') || die();
 
-class send_emails_teachers_format_uvirtual extends \core\task\scheduled_task {
-
+class send_emails_teachers_format_uvirtual extends \core\task\scheduled_task
+{
     /**
      * Name for this task.
      *
      * @return string
      */
-    public function get_name() {
+    public function get_name(): string
+    {
         return 'Enviar correos de informes';
     }
 
     /**
      * Run task for loading keycloak userids into user profile.
+     * @throws dml_exception
      */
-    public function execute() {
+    public function execute(): void
+    {
         global $DB, $OUTPUT;
 
-        
         $blockconfig = get_config('block_grade_overview');
 
         $ahora = time();
@@ -46,19 +52,19 @@ class send_emails_teachers_format_uvirtual extends \core\task\scheduled_task {
         $stardate = $ahora;
         $enddate = $ahora - $calculodias;
 
-
         $send = $blockconfig->enableemailreport;
 
-
         if (!empty($send) || true) {
+
             $params = [
                 'startdate' => $stardate,
-                'enddate'   => $enddate,
-                'visible'   => 1,
-                'courseid'  => 1
+                'enddate' => $enddate,
+                'visible' => 1,
+                'courseid' => 1
             ];
+
             if (!empty($testcourse)) {
-                $sqltestcourse = "AND c.id IN (".$testcourse.")";
+                $sqltestcourse = "AND c.id IN (" . $testcourse . ")";
             }
 
             $sql = "SELECT c.*
@@ -69,31 +75,35 @@ class send_emails_teachers_format_uvirtual extends \core\task\scheduled_task {
                     AND c.id != :courseid
                     AND c.format = 'uvirtual'
                     $sqltestcourse";
+
             $courses = $DB->get_records_sql($sql, $params);
+
             if (!empty($courses)) {
+
                 $mensaje = ' Envio de correos de informes profesores comenzando...';
                 mtrace($mensaje);
 
                 foreach ($courses as $course) {
                     $teacherid = array_keys(get_archetype_roles('teacher'));
                     $editingteacherid = array_keys(get_archetype_roles('editingteacher'));
-                    $teacherroleids = array_merge($editingteacherid, $teacherid);
-                    $teachers = \course_info::get_course_tutor($course->id, 'u.*', array_values($teacherroleids));
-                    $istfm = format_uvirtual_get_course_metadata($course->id , 'Otros campos', 'typecourse', '4' );
+                    $rdirecprogramid = array_keys(get_archetype_roles('rdirecprogram'));
+                    $teacherroleids = array_merge($editingteacherid, $teacherid, $rdirecprogramid);
+                    $teachers = course_info::get_course_tutor($course->id, 'u.*', array_values($teacherroleids));
+                    $istfm = format_uvirtual_get_course_metadata($course->id, 'Otros campos', 'typecourse', '4');
 
                     if (!empty($teachers)) {
                         foreach ($teachers as $teacher) {
                             $context = format_uvirtual_get_teacher_pendientes_context($course, $teacher);
-                            if($istfm){
-                                if (!$context['assign_tfm']  ) {
+                            if ($istfm) {
+                                if (!$context['assign_tfm']) {
                                     continue;
                                 }
                             } else {
-                                if (!$context['displayconsultas'] && !$context['displayretos'] ) {
+                                if (!$context['displayconsultas'] && !$context['displayretos']) {
                                     continue;
                                 }
                             }
-                            
+
                             $htmlemail = $OUTPUT->render_from_template('format_uvirtual/email/teachers_email', $context);
                             if (!empty($htmlemail)) {
                                 $htmlemail = str_replace('ú', ' ', $htmlemail);
@@ -107,7 +117,7 @@ class send_emails_teachers_format_uvirtual extends \core\task\scheduled_task {
                                 block_grade_overview_send_email($mailobject);
                             }
                         }
-                    } 
+                    }
                 }
                 $mensaje = ' Envio de correos de informes profesores finalizando...';
                 mtrace($mensaje);
