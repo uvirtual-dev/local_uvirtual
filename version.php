08822041a7f4bec95b0729346e06bf5849389f20
Miguel Villegas <mihivira@gmail.com>
2024-11-18 11:21:45 -0600

    update version

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_uvirtual';
 $plugin->release = '3.11';
-$plugin->version = 20241009003;
+$plugin->version = 20241118001;
 $plugin->requires = 2020110900;
 $plugin->maturity = MATURITY_ALPHA;
